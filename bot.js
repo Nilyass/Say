17b5fdb0ad88f8e6bd009cbd5359cdb98040aba8
Mamas159 <gevogame@gmail.com>
2019-05-05 01:15:07 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const embed = new Discord.RichEmbed()
+const prefix = "!"
+
+client.on('message', message => {
+  let args = message.content.split(" ").slice(1);
+ 
+  if(message.content.startsWith(prefix + "say"))  {
+         message.delete()
+         const embed = new Discord.RichEmbed()
+         .setDescription(args.join(" "))
+         .setColor(0xff0000)
+    message.channel.sendEmbed(embed);
+ 
+     }
+ 
+});
+
+client.login("NTU1NzE1ODA3NjExNDUzNDQy.XM4p6A.FmkFB7_bEgGGgUbuDuhGoMZ9YA4");
