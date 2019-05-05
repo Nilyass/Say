d1c002eac067add5399e01d92b37d829bbfa5f51
Mamas159 <gevogame@gmail.com>
2019-05-05 01:29:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const Discord = require("discord.js");
 
-if (command === 'say') {
-    let say = args.join(' ');
-    msg.delete();
-    if(!msg.member.permissions.has('ADMINISTRATION') ) return msg.reply('**YOU CANT USE THIS COMMAND !')
-    msg.channel.send(say);
+module.exports.run = async (bot, message, args) => {
+
+if (fromID === me && text.includes("!say") ) {
+bot.sendMessage({ to: channel, message: txt.replace('!say ','') });
 }
 
+module.exports.help = {
+name: "say"
+}
 client.login("NTU1NzE1ODA3NjExNDUzNDQy.XM4p6A.FmkFB7_bEgGGgUbuDuhGoMZ9YA4");
