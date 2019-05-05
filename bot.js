6254f1975900aa27fa76dd589c305c520457d8f3
Mamas159 <gevogame@gmail.com>
2019-05-05 01:24:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,16 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const embed = new Discord.RichEmbed()
-const prefix = "!"
 
-client.on('message', message => {
-  let args = message.content.split(" ").slice(1);
- 
-  if(message.content.startsWith(prefix + "say"))  {
-         message.delete()
-         const embed = new Discord.RichEmbed()
-         .setDescription(args.join(" "))
-         .setColor("RANDOM")
-    message.channel.sendEmbed(embed);
- 
-     }
- 
-});
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+
+}
+
+module.exports.help = {
+  name: "say"
+}
 
 client.login("NTU1NzE1ODA3NjExNDUzNDQy.XM4p6A.FmkFB7_bEgGGgUbuDuhGoMZ9YA4");
